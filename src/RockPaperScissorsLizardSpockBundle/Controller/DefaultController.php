fcb86ee743e905f3900af1491d7c0defd5a0d252
bluedot74 <blue74dot@gmail.com>
2016-04-28 17:30:58 -0500

    Adding empty bundle

---
+++ src/RockPaperScissorsLizardSpockBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace RockPaperScissorsLizardSpockBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('RockPaperScissorsLizardSpockBundle:Default:index.html.twig');
+    }
+}
---
+++ src/RockPaperScissorsLizardSpockBundle/RockPaperScissorsLizardSpockBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RockPaperScissorsLizardSpockBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RockPaperScissorsLizardSpockBundle extends Bundle
+{
+}
