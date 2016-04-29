3ec51ef967dd3f763be1a7d860b5813268f83d79
bluedot74 <blue74dot@gmail.com>
2016-04-28 19:20:22 -0500

    Filled out test stubs and added more for Lose and Draw outcomes

--- src/JoshWillis/RockPaperScissorsLizardSpockBundle/Tests/GameTest.php
+++ src/JoshWillis/RockPaperScissorsLizardSpockBundle/Tests/GameTest.php
@@ -2,9 +2,14 @@
 namespace JoshWillis\RockPaperScissorsLizardSpockBundle\Tests;
 
 
+use JoshWillis\RockPaperScissorsLizardSpockBundle\Characters\Lizard;
+use JoshWillis\RockPaperScissorsLizardSpockBundle\Characters\Paper;
+use JoshWillis\RockPaperScissorsLizardSpockBundle\Characters\Rock;
 use JoshWillis\RockPaperScissorsLizardSpockBundle\Characters\Scissors;
 use JoshWillis\RockPaperScissorsLizardSpockBundle\Characters\Spock;
 use JoshWillis\RockPaperScissorsLizardSpockBundle\Game;
+use JoshWillis\RockPaperScissorsLizardSpockBundle\Outcomes\Draw;
+use JoshWillis\RockPaperScissorsLizardSpockBundle\Outcomes\Lose;
 use JoshWillis\RockPaperScissorsLizardSpockBundle\Outcomes\Win;
 use JoshWillis\RockPaperScissorsLizardSpockBundle\Player;
 
@@ -12,7 +17,6 @@ class GameTest extends \PHPUnit_Framework_TestCase
 {
     public function testSpockBeatsScissors()
     {
-        $test = new Scissors();
         $game = new Game(
             new Player(new Spock, 'Player'),
             new Player(new Scissors, 'Computer')
@@ -24,47 +28,131 @@ class GameTest extends \PHPUnit_Framework_TestCase
 
     public function testSpockBeatsRock()
     {
+        $game = new Game(
+            new Player(new Spock, 'Player'),
+            new Player(new Rock, 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
 
     public function testScissorsBeatsPaper()
     {
+        $game = new Game(
+            new Player(new Scissors(), 'Player'),
+            new Player(new Paper(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
 
     public function testScissorsBeatsLizard()
     {
+        $game = new Game(
+            new Player(new Scissors, 'Player'),
+            new Player(new Paper, 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
 
     public function testPaperBeatsRock()
     {
+        $game = new Game(
+            new Player(new Paper(), 'Player'),
+            new Player(new Rock(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
 
     public function testPaperBeatsSpock()
     {
+        $game = new Game(
+            new Player(new Paper(), 'Player'),
+            new Player(new Spock(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
 
     public function testRockBeatsLizard()
     {
+        $game = new Game(
+            new Player(new Rock(), 'Player'),
+            new Player(new Lizard(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
 
     public function testRockBeatsScissors()
     {
+        $game = new Game(
+            new Player(new Rock(), 'Player'),
+            new Player(new Scissors(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
 
     public function testLizardBeatsSpock()
     {
+        $game = new Game(
+            new Player(new Lizard(), 'Player'),
+            new Player(new Spock(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
 
     public function testLizardBeatsPaper()
     {
+        $game = new Game(
+            new Player(new Lizard(), 'Player'),
+            new Player(new Paper(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
 
+        $this->assertInstanceOf(Win::class,$outcome);
     }
+
+    public function testSameCharactersResultInDraw(){
+        $game = new Game(
+            new Player(new Paper(), 'Player'),
+            new Player(new Paper(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
+
+        $this->assertInstanceOf(Draw::class,$outcome);
+
+        $game = new Game(
+            new Player(new Spock(), 'Player'),
+            new Player(new Spock(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
+
+        $this->assertInstanceOf(Draw::class,$outcome);
+    }
+
+    public function testLosingReturnsLoseOutcome(){
+        $game = new Game(
+            new Player(new Paper(), 'Player'),
+            new Player(new Lizard(), 'Computer')
+        );
+        $outcome = $game->getOutcome();
+
+        $this->assertInstanceOf(Lose::class,$outcome);
+    }
+
+
 
 }
